7897d5a8118d9050456b95388e155c1d68678fcf
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-17 00:50:44 -0400

    Questions and answers loaded from JSON. Logic pending.

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,23 @@
 (function(){
 
-	// script goes here
+	var app = angular.module('myQuiz',[]);
+
+  app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+    $scope.score = 1;
+    $scope.activeQuestion = -1;
+    $scope.activeQuestionAnswered = 0;
+    $scope.percentage = 0;
+
+    $http.get('quiz_data.json').then(function(quizData) {
+      $scope.myQuestions = quizData.data;
+      $scope.totalQuestions = $scope.myQuestions.length;
+    });
+
+    $scope.selectAnswer = function(qIndex, aIndex) {
+      var questionState = $scope.myQuestions[qIndex].questionState;
+
+      if( questionState != 'answered')
+    }
+  }]);
 
 })();
